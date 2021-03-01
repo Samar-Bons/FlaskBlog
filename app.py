1defc29561dd842cd89a9cc174b63a9d3929346c
Samarjit Singh Bons <samarjitsinghbons@gmail.com>
2021-02-28 22:15:04 -0600

    delete added

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
-#Fucntion to connect to db and return conn object
+#Function to connect to db and return conn object
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
@@ -42,7 +42,7 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-
+#flash not working
         if not title:
             flash('Title is needed!')
         else:
@@ -55,6 +55,36 @@ def create():
             return redirect(url_for('index'))
 
     return render_template('create.html')
+  
+@app.route('/<int:id>/edit', methods=('GET','POST'))
+def edit(id):
+	post = get_post(id)
+	
+	if request.method == 'POST':
+		title = request.form['title']
+		content = request.form['content']
+		
+		if not title:
+			flash('Title is required!')
+		else:
+			conn = get_db_connection()
+			conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?',(title,content,id))
+			conn.commit()
+			conn.close()
+			return redirect(url_for('index'))
+			
+						
+	return render_template('edit.html', post=post)
+	
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+	post = get_post(id)
+	conn = get_db_connection()
+	conn.execute('DELETE FROM posts WHERE id=?',(id,))
+	conn.commit()
+	conn.close()
+	flash('"{}" was successfully deleted!'.format(post['title']))
+	return redirect(url_for('index'))
 
 
     
