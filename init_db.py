2c94eaed81e90c246cff5c15d997b56b9baf8b24
Samarjit Singh Bons <samarjitsinghbons@gmail.com>
2021-03-03 02:07:45 -0500

    comments added

--- init_db.py
+++ init_db.py
@@ -1,10 +1,15 @@
+#Module to initialize a db named "database.db" which will store 
+#post IDs, time_created, Title, and Post content
+
 import sqlite3
 
 connection = sqlite3.connect('database.db')
 
-
+# Executing schema.sql script to initialize our db
 with open('schema.sql') as f:
     connection.executescript(f.read())
+
+# Prepopulating the database for developmental purposes
 
 cur = connection.cursor()
 
--- schema.sql
+++ schema.sql
@@ -1,6 +1,12 @@
+/* A simple sql script to init a new database for our application */
+
 DROP TABLE IF EXISTS posts;
 
 CREATE TABLE posts (
+
+    /* We will only be concerned with adding a title and content by the user 
+        since 'id' and 'created' are expected to autogenerate */
+        
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title TEXT NOT NULL,
