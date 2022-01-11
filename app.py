0a6d18697b0bbc54a96af6ca90b38a8f10b97eec
Samarjit Singh Bons <samarjitsingh.bons@mavs.uta.edu>
2022-01-11 10:09:55 -0600

    ok

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from werkzeug.exceptions import abort
 
 #Function to connect to db and return conn object
 def get_db_connection():
+
     conn = sqlite3.connect('database.db')
 
     #Making every row behave as a dictionary
@@ -13,6 +14,7 @@ def get_db_connection():
 #Function to fetch a post using given post_id.
 #returns a row as a dictionary object
 def get_post(post_id):
+
     conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?',(post_id,)).fetchone()
     conn.close()
@@ -63,46 +65,46 @@ def create():
             return redirect(url_for('index'))
 
     return render_template('create.html')
-  
+
 
 #View function to edit an existing post
 @app.route('/<int:id>/edit', methods=('GET','POST'))
 def edit(id):
-	post = get_post(id)
-	
-	if request.method == 'POST':
-		title = request.form['title']
-		content = request.form['content']
-		
-		if not title:
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        
+        if not title:
             #reload edit page with a flash warning
-			flash('Title is required!')
-		else:
+            flash('Title is required!')
+        else:
             #update sql db
-			conn = get_db_connection()
-			conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?',(title,content,id))
-			conn.commit()
-			conn.close()
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?',(title,content,id))
+            conn.commit()
+            conn.close()
             #redirect to homepage
-			return redirect(url_for('index'))
-			
+            return redirect(url_for('index'))
+            
 						
-	return render_template('edit.html', post=post)
+    return render_template('edit.html', post=post)
 	
 #View function to delete an existing post
 @app.route('/<int:id>/delete', methods=('POST',))
 def delete(id):
     #Retrieve post to display a flash message later with Post title
-	post = get_post(id)
-	conn = get_db_connection()
-    
+    post = get_post(id)
+    conn = get_db_connection()
+
     #delete row from sql db
-	conn.execute('DELETE FROM posts WHERE id=?',(id,))
-	conn.commit()
-	conn.close()
+    conn.execute('DELETE FROM posts WHERE id=?',(id,))
+    conn.commit()
+    conn.close()
     #Flash message with title retrived earlier
-	flash('"{}" was successfully deleted!'.format(post['title']))
-	#return to homepage
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    #return to homepage
     return redirect(url_for('index'))
 
 
